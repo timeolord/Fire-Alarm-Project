cc019773a7947a12f8f5a6819ff437184ad1a60a
Allan <timeolord6677@gmail.com>
2021-01-30 15:43:34 -0500

    AudioProcessor.java

--- app/src/main/java/com/mchacks/firealarmproject/AudioProcessor.java
+++ app/src/main/java/com/mchacks/firealarmproject/AudioProcessor.java
@@ -1,11 +1,43 @@
 package com.mchacks.firealarmproject;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
+import androidx.core.app.NotificationCompat;
+
+import static com.mchacks.firealarmproject.App.CHANNEL_ID;
+
 public class AudioProcessor extends Service {
     public AudioProcessor() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        String input = intent.getStringExtra("Input Extra");
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID).setContentTitle("Audio Processor")
+                .setContentText(input).setSmallIcon(R.drawable.ic_hearing_disabled).setContentIntent(pendingIntent).build();
+
+
+        startForeground(1, notification);
+
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 
     @Override
--- app/src/main/java/com/mchacks/firealarmproject/MainActivity.java
+++ app/src/main/java/com/mchacks/firealarmproject/MainActivity.java
@@ -2,12 +2,34 @@ package com.mchacks.firealarmproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editTextInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }}
+
+        editTextInput = findViewById(R.id.edit_text_input);
+    }
+
+    public void startService(View v){
+        String input = editTextInput.getText().toString();
+
+        Intent serviceIntent = new Intent(this, AudioProcessor.class);
+        serviceIntent.putExtra("Input Extra", input);
+
+        startService(serviceIntent);
+    }
+
+    public void stopService(View v){
+        Intent serviceIntent = new Intent(this, AudioProcessor.class);
+        stopService(serviceIntent);
+    }
+
+}
